970fdcbec3f144f36cee6b1278da13685e7cb06b
cormac <cormacflanagan12@gmail.com>
2025-10-18 16:38:49 +1000

    Redesigned Model to Unet3D
    - Previous Model was based on a 3D version of my implementation of UNet
    - Updated to the spec provided
    https://arxiv.org/pdf/1802.10508v1

--- recognition/Unet3D-4809765/modules.py
+++ recognition/Unet3D-4809765/modules.py
@@ -1,15 +1,41 @@
 import torch
 import torch.nn as nn
+from torch.nn.functional import interpolate
 
 
-class DoubleConv3D(nn.Module):
-    def __init__(self, in_channels, out_channels):
+class ContextModule(nn.Module):
+    def __init__(self, channels) -> None:
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(channels),
             nn.ReLU(inplace=True),
-            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv3d(channels, channels, kernel_size=3, padding=1),
+            nn.Dropout3d(0.3),
+            nn.BatchNorm3d(channels),
             nn.ReLU(inplace=True),
+            nn.Conv3d(channels, channels, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class Upsample(nn.Module):
+    def __init__(self, in_chanels, out_channels) -> None:
+        super().__init__()
+        self.conv = nn.Conv3d(in_chanels, out_channels, 3, 1, 1)
+
+    def forward(self, x):
+        x = interpolate(x, scale_factor=2, mode="nearest")
+        return self.conv(x)
+
+
+class Localization(nn.Module):
+    def __init__(self, in_channels, out_channels) -> None:
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv3d(in_channels, in_channels, 3, 1, 1),
+            nn.Conv3d(in_channels, out_channels, 1),
         )
 
     def forward(self, x):
@@ -19,41 +45,66 @@ class DoubleConv3D(nn.Module):
 class Unet3D(nn.Module):
     def __init__(self, in_channels=1, num_classes=3):
         super().__init__()
-        self.c1 = DoubleConv3D(in_channels, 64)
-        self.maxpool = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.c2 = DoubleConv3D(64, 128)
-        # maxpool
-        self.c3 = DoubleConv3D(128, 256)
-        # maxpool
-        self.c4 = DoubleConv3D(256, 512)
-        # maxpool
-        self.c5 = DoubleConv3D(512, 1024)  # Turning Point merges with c4
-        self.up3 = nn.ConvTranspose3d(1024, 1024, kernel_size=2, stride=2)
-        self.c6 = DoubleConv3D(1024 + 512, 512)  # Modern
-        self.up2 = nn.ConvTranspose3d(512, 512, kernel_size=2, stride=2)
-        self.c7 = DoubleConv3D(512 + 256, 256)
-        self.up1 = nn.ConvTranspose3d(256, 256, kernel_size=2, stride=2)
-        self.c8 = DoubleConv3D(256 + 128, 128)
-        self.up0 = nn.ConvTranspose3d(128, 128, kernel_size=2, stride=2)
-        self.c9 = DoubleConv3D(128 + 64, 64)
+        self.conv1 = nn.Conv3d(in_channels, 16, kernel_size=3, padding=1)
+        self.tex1 = ContextModule(16)
 
-        self.final = nn.Conv3d(64, num_classes, kernel_size=1)
+        self.conv2 = nn.Conv3d(16, 32, 3, 2, 1)
+        self.tex2 = ContextModule(32)
 
-    def forward(self, x):
-        r0 = self.c1(x)
-        r1 = self.c2(self.maxpool(r0))
-        r2 = self.c3(self.maxpool(r1))
-        r3 = self.c4(self.maxpool(r2))
-        r4 = self.c5(self.maxpool(r3))
+        self.conv3 = nn.Conv3d(32, 64, 3, 2, 1)
+        self.tex3 = ContextModule(64)
 
-        r3 = torch.cat([r3, self.up3(r4)], dim=1)
-        r3 = self.c6(r3)
+        self.conv4 = nn.Conv3d(64, 128, 3, 2, 1)
+        self.tex4 = ContextModule(128)
 
-        r2 = torch.cat([r2, self.up2(r3)], dim=1)
-        r2 = self.c7(r2)
+        self.conv5 = nn.Conv3d(128, 256, 3, 2, 1)
+        self.tex5 = ContextModule(256)
 
-        r1 = torch.cat([r1, self.up1(r2)], dim=1)
-        r1 = self.c8(r1)
+        self.up5 = Upsample(256, 128)
 
-        r0 = torch.cat([r0, self.up0(r1)], dim=1)
-        return self.final(self.c9(r0))
+        self.loc4 = Localization(256, 128)
+        self.up4 = Upsample(128, 64)
+
+        self.loc3 = Localization(128, 64)
+        self.seg3 = nn.Conv3d(64, num_classes, 1)
+        self.up3 = Upsample(64, 32)
+
+        self.loc2 = Localization(64, 32)
+        self.seg2 = nn.Conv3d(32, num_classes, 1)
+        self.up2 = Upsample(32, 16)
+
+        self.convF = nn.Conv3d(32, 32, 3, 1, 1)
+        self.final = nn.Softmax(0)
+
+    def forward(self, r0):
+        r0 = self.conv1(r0)  # N
+        r0 = r0 + self.tex1(r0)
+        r1 = self.conv2(r0)  # N/2
+        r1 = r1 + self.tex2(r1)
+        r2 = self.conv3(r1)  # N/4
+        r2 = r2 + self.tex3(r2)
+        r3 = self.conv4(r2)  # N/8
+        r3 = r3 + self.tex4(r3)
+        r4 = self.conv5(r3)  # N/16
+        r4 = r4 + self.tex5(r4)
+        r3 = torch.cat([r3, self.up5(r4)], dim=1)
+        del r4
+        torch.cuda.empty_cache()
+        r3 = self.up4(self.loc4(r3))
+        r2 = torch.cat([r3, r2], dim=1)
+        del r3
+        torch.cuda.empty_cache()
+        r2 = self.loc3(r2)
+        r1 = torch.cat([r1, self.up3(r2)], dim=1)
+        r1 = self.loc2(r1)
+        r0 = torch.cat([r0, self.up2(r1)], dim=1)
+        r1 = self.seg2(r1)
+        r0 = self.seg2(self.convF(r0))
+
+        r0 = r0 + interpolate(
+            r1 + interpolate(self.seg3(r2), scale_factor=2, mode="nearest"),
+            scale_factor=2,
+            mode="nearest",
+        )
+
+        return self.final(r0)
